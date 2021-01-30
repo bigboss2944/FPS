5a1215e6811447e8265b3b08250084ca91eb5229
fabrice-noel29 <fabrice.noel29@gmail.com>
2021-01-30 19:11:49 +0100

    Added fire script

---
+++ Assets/FPS KIT 3D/Prefabs/PistolScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistolScript : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform posTir;
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject go = Instantiate(projectile, posTir.position, Quaternion.identity);
+
+            go.GetComponent<Rigidbody>().AddForce(posTir.forward * force);
+
+            Destroy(go, 2);
+        }
+    }
+}
---
+++ Assets/FPS KIT 3D/Prefabs/TurrelScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurrelScript : MonoBehaviour
+{
+    public Transform target;
+    public GameObject bullet;
+    public float force;
+    public float reloadTime;
+    public Transform posTir;
+    public GameObject canon;
+    bool canShot = true;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(target.position.x, canon.transform.position.y, target.position.z);
+        canon.transform.LookAt(targetPosition);
+
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        if(canShot && distance < 1000000)
+        {
+            canShot = false;
+
+            GameObject go = Instantiate(bullet, posTir.position, Quaternion.identity);
+
+            go.GetComponent<Rigidbody>().AddForce(posTir.forward * force);
+
+            Destroy(go, 2);
+
+            StartCoroutine("Reload");
+
+
+        }
+
+        
+    }
+
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(reloadTime);
+        canShot = true;
+    }
+}
